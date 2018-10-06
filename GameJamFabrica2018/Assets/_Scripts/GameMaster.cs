d064e4d68bb1f80603e9a78be3798a5febe26aae
Filipe Leal <leal.filipe@live.com>
2018-10-06 14:11:34 -0600

    Camera Shake

--- GameJamFabrica2018/Assets/_Scripts/GameMaster.cs
+++ GameJamFabrica2018/Assets/_Scripts/GameMaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,26 +10,48 @@ public class GameMaster : MonoBehaviour
     public int Ouros;
     public float TempoTotal = 30f;
 
+    [Header("Camera")]
+    public float TempoEntreShakes = 10f;
+    public float ShakeTime = 1.5f;
+
+    private bool _shaking = false;
+    private float _tempoParaShake;
+
     private bool _gameOver = false;
+
+    public CinemachineVirtualCamera MainCamera;
+    private CinemachineBasicMultiChannelPerlin _noiseSettings;
 
     [Header("HUD")]
     public TextMeshProUGUI OurosText;
     public TextMeshProUGUI TempoText;
 
-
     // Use this for initialization
     void Start()
     {
-
         Ouros = 0;
         OurosText.text = "$0";
+        _tempoParaShake = TempoEntreShakes;
+        _noiseSettings = MainCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     void Update()
     {
         if (_gameOver)
             return;
+
         TempoTotal -= Time.deltaTime;
+
+        _tempoParaShake -= Time.deltaTime;
+        if (_tempoParaShake <= 0f && !_shaking)
+        {
+            Shake();
+        }
+
+        if (_tempoParaShake <= 0f && _shaking)
+        {
+            StopShake();
+        }
 
         var format = "00";
         if (TempoTotal < 10)
@@ -56,5 +79,25 @@ public class GameMaster : MonoBehaviour
     {
         _gameOver = true;
         Debug.Log("GAME OVER");
+    }
+
+    void Shake()
+    {
+        _shaking = true;
+
+        _noiseSettings.m_AmplitudeGain = 0.1f;
+        _noiseSettings.m_FrequencyGain = 2;
+
+        _tempoParaShake = ShakeTime;
+    }
+
+    void StopShake()
+    {
+        _noiseSettings.m_AmplitudeGain = 0;
+        _noiseSettings.m_FrequencyGain = 0;
+
+        _shaking = false;
+
+        _tempoParaShake = TempoEntreShakes;
     }
 }
