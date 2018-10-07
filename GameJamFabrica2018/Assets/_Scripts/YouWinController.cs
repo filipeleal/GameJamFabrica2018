17cadf9ea48d50f2772259bfd70aa11c402f0e89
Filipe Leal <leal.filipe@live.com>
2018-10-07 15:12:29 -0600

    texto You Win

---
+++ GameJamFabrica2018/Assets/_Scripts/YouWinController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class YouWinController : MonoBehaviour {
+    public TextMeshProUGUI FortunaOuVida;
+    public TextMeshProUGUI Fortuna;
+
+    private int _ouros;
+	// Use this for initialization
+	void Start () {
+        _ouros = PlayerPrefs.GetInt("Ouros");
+
+        if(_ouros == 0)
+        {
+            FortunaOuVida.text = "É melhor ter uma vida pobre do que um milhonário soterrado!";
+            Fortuna.enabled = false;
+        }
+        else
+        {
+            FortunaOuVida.text = "Sua fortuna é de:";
+            Fortuna.enabled = true;
+            Fortuna.text = _ouros.ToString("C");
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
