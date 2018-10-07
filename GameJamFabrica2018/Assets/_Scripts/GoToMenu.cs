56c0897bf9b1faef3c97f78950d4f0053d8e8476
Filipe Leal <leal.filipe@live.com>
2018-10-06 21:17:40 -0600

    Menu flow

---
+++ GameJamFabrica2018/Assets/_Scripts/GoToMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour {
+
+    public TextMeshProUGUI ContinueText;
+
+    private bool _firstKey;
+
+
+    void Start()
+    {
+        ContinueText.enabled = false;
+        _firstKey = false;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            if(_firstKey)
+                SceneManager.LoadScene("Menu");
+
+            ContinueText.enabled = true;
+            _firstKey = true;
+        }	
+	}
+}
